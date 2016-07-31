f5540f394b9cbcaae0f1c36fe561d55408322704
DuanPengFly <304124576@qq.com>
2016-07-31 21:19:22 +0800

    add project

---
+++ FGLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FGLog
+//
+//  Created by 段鹏飞 on 16/7/31.
+//  Copyright © 2016年 SuperFG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FGLogTests/FGLogTests.m
@@ -0,0 +1,39 @@
+//
+//  FGLogTests.m
+//  FGLogTests
+//
+//  Created by 段鹏飞 on 16/7/31.
+//  Copyright © 2016年 SuperFG. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FGLogTests : XCTestCase
+
+@end
+
+@implementation FGLogTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
